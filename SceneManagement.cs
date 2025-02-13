3d34ddf502ae3161ccf634c9467845954c0cff09
kadir emir yücel <kadiremiryucel123@gmail.com>
2025-02-13 13:34:03 +0300

    Create  SceneManagement.cs

---
+++ SceneManagement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+using static Unity.Burst.Intrinsics.X86;
+
+public class SceneManagement : MonoBehaviour
+{
+  
+    public string X = "Bedroom";
+    public GameObject credits;
+
+    public void toggleCredits()
+    {
+        credits.SetActive(!credits.activeSelf);
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+
+    public void bedroom()
+    {
+        SceneManager.LoadScene(X);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("LabirentExit"))
+        {
+            SceneManager.LoadScene(X);
+           
+        }
+    }
+
+    public void gokdelenFanus()
+    {
+        SceneManager.LoadScene("GokdelenFanus");
+    }
+
+    public void okyanusFanus()
+    {
+        SceneManager.LoadScene("OkyanusFanus");
+    }
+}
